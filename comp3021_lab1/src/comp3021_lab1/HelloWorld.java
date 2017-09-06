dcb6203eca2123ffeaba68775938a7a7d293326b
Kristian Suhartono <blackxephos2@gmail.com>
2017-09-06 09:13:43 +0800

    update to task for lab 1 requirements

--- comp3021_lab1/src/comp3021_lab1/HelloWorld.java
+++ comp3021_lab1/src/comp3021_lab1/HelloWorld.java
@@ -2,6 +2,7 @@ package comp3021_lab1;
 
 public class HelloWorld {
 	public static void main (String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Name: Kristian Suhartono");
+		System.out.println("SID: 20313207");
 	}
 }
